499d870333284a6221b902d509395f0665c1963c
wangshuo <2192595668@qq.com>
2025-03-05 20:27:12 +0800

    888

--- src/main/java/com/customer/service/Impl/UserServiceImpl.java
+++ src/main/java/com/customer/service/Impl/UserServiceImpl.java
@@ -48,17 +48,46 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
-    public String getOpenid(String code) {
-        Map<String, String> map = new HashMap<>();
-        map.put("appid", weChatProperties.getAppid());
-        map.put("secret", weChatProperties.getSecret());
-        map.put("js_code", code);
-        map.put("grant_type", "authorization_code");
-        String json = HttpClientUtil.doGet(WX_LOGIN, map);
-        JSONObject jsonObject = JSON.parseObject(json);
-        String openid = jsonObject.getString("openid");
-        return openid;
-    }
+//    public String getOpenid(String code) {
+//        Map<String, String> map = new HashMap<>();
+//        map.put("appid", weChatProperties.getAppid());
+//        map.put("secret", weChatProperties.getSecret());
+//        map.put("js_code", code);
+//        map.put("grant_type", "authorization_code");
+//        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+//        JSONObject jsonObject = JSON.parseObject(json);
+//        String openid = jsonObject.getString("openid");
+//        return openid;
+//    }
+        public String getOpenid(String code) {
+            try {
+                // 微信小程序登录凭证校验接口URL
+                String url = "https://api.weixin.qq.com/sns/jscode2session";
+                // 准备参数
+                Map<String, String> params = new HashMap<>();
+                params.put("appid", weChatProperties.getAppid());
+                params.put("secret", weChatProperties.getSecret());
+                params.put("js_code", code);
+                params.put("grant_type", "authorization_code");
+
+                // 发送 GET 请求
+                String response = HttpClientUtil.doGet(url, params);
+                JSONObject jsonObject = JSONObject.parseObject(response);
+                // 检查错误码
+                if (jsonObject.containsKey("errcode")) {
+                    // 处理错误情况
+                    int errcode = jsonObject.getInteger("errcode");
+                    String errmsg = jsonObject.getString("errmsg");
+                    throw new RuntimeException("Error from WeChat: " + errmsg + " (errcode: " + errcode + ")");
+                }
+                // 获取 openid
+                return jsonObject.getString("openid");
+            } catch (Exception e) {
+                // 记录日志或进行其他异常处理
+                // logger.error("获取openid失败", e);
+                throw new RuntimeException("获取openid失败", e);
+            }
+        }
     /**
      * 修改用户信息
      *
