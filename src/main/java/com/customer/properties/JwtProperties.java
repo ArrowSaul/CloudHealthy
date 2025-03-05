3158f2b4a0530304fd40596e9e955dac469af180
wangshuo <2192595668@qq.com>
2025-03-05 20:45:28 +0800

    999

--- src/main/java/com/customer/properties/JwtProperties.java
+++ src/main/java/com/customer/properties/JwtProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "attend.jwt")
+@ConfigurationProperties(prefix = "customer.jwt")
 @Data
 public class JwtProperties {
 
