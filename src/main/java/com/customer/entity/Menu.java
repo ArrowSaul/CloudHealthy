96e71476bc89623d2686a7bbebd52a350a7f8973
wangshuo <2192595668@qq.com>
2025-03-05 22:08:37 +0800

    菜单实例

--- src/main/java/com/customer/entity/Menu.java
+++ src/main/java/com/customer/entity/Menu.java
@@ -1,4 +1,32 @@
 package com.customer.entity;
 
-public class Menu {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu implements Serializable {
+    /**
+     * 序列化版本唯一标识符
+     * 用于在序列化和反序列化过程中确保类的版本一致性和兼容性
+     */
+    private static final long serialVersionUID = 1L;
+    private Long id; // 主键
+    private String name; // 菜单名称
+    private Integer sort; // 顺序
+    private Integer status; // 状态 0:禁用，1:启用
+    private BigDecimal price; // 金额
+    private String image; // 图片路径
+    private LocalDateTime createTime; // 创建时间
+    private LocalDateTime updateTime; // 更新时间
+    private Long createUser; // 创建人
+    private Long updateUser; // 修改人
 }
