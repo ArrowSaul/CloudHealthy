015ca3322d0f14f1e3bd6ce2f9d83a1f75ed5a80
wangshuo <2192595668@qq.com>
2025-03-18 21:40:46 +0800

    222

--- src/main/java/com/customer/config/WebMvcConfiguration.java
+++ src/main/java/com/customer/config/WebMvcConfiguration.java
@@ -41,7 +41,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenUserInterceptor)
-                .addPathPatterns("")
+                .addPathPatterns("/user/**")       // 拦截所有以 /user 开头的接口
                 .excludePathPatterns("/user/user/login", "/**/");
     }
 
